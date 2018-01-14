4ec05dbd321e2c1bed65951dabfcfa3da457e05d
liuziyang <liuziyang243@hotmail.com>
2018-01-14 20:23:07 +0800

    根据第2章内容新建第一个微服务程序。

--- src/main/java/demo/msa/msahello/HelloController.java
+++ src/main/java/demo/msa/msahello/HelloController.java
@@ -1,8 +1,18 @@
 package demo.msa.msahello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by liuziyang
  * Create date: 2018/1/14
  */
+
+@RestController
 public class HelloController {
+    @RequestMapping(method = RequestMethod.GET,path = "/hello")
+    public String hello() {
+        return "hello world";
+    }
 }
